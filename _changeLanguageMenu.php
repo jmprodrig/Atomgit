38d4b05d7b662c87a4ea2b0d0b376e2288deb821
jmprodrig <53745968+jmprodrig@users.noreply.github.com>
2023-02-10 17:38:30 +0000

    Update _changeLanguageMenu.php

--- _changeLanguageMenu.php
+++ _changeLanguageMenu.php
@@ -12,15 +12,23 @@
       <h2><?php echo __('Language') ?></h2>
     </div>
 
-    <div class="top-dropdown-body">
-      <ul>
-        <?php foreach ($langCodes as $value): ?>
-          <li<?php if ($sf_user->getCulture() == $value): ?> class="active"<?php endif; ?>>
-            <?php echo link_to(format_language($value, $value), array('sf_culture' => $value) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
+   <div class="top-dropdown-body">
+  <ul>
+    <?php
+    // reordenando a lista para que o "pt" seja o primeiro elemento
+    $langCodes = array_diff($langCodes, array('pt'));
+    array_unshift($langCodes, 'pt');
+    ?>
+    <?php foreach ($langCodes as $value): ?>
+      <li<?php if ($sf_user->getCulture() == $value): ?> class="active"<?php endif; ?>>
+        <?php
+        $formattedLanguage = ucwords(format_language($value, $value));
+        echo link_to($formattedLanguage, array('sf_culture' => $value) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll())
+        ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
 
     <div class="top-dropdown-bottom"></div>
 
